d4c6252685f32f52883f616cf42c4c63544c2217
chaitanya peddada <chaitu.gvpe@gmail.com>
2025-11-06 14:01:51 +0530

    product route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar"; // <-- ensure this matches src/components/NavBar.jsx
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 import BranchList from "./components/BranchList";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-800">
-      <Header />
-      <Gallery />
-      <BranchList />
-      <Contact />
-      <Footer />
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Header />
+        <Routes>
+          <Route path="services" element={<Gallery />} />
+          <Route path="branches" element={<BranchList />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
